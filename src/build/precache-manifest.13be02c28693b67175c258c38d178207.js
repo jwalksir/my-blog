fbc6a3bb019921c5f09ec52834cd8e401bc33202
Justin Walker <justincolewalker@gmail.com>
2020-06-30 10:27:01 -0400

    First Commit

---
+++ src/build/precache-manifest.13be02c28693b67175c258c38d178207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb914af40651c88eb328b7acb0289577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd756423a02dff3a3ec6",
+    "url": "/static/css/main.43cf58e8.chunk.css"
+  },
+  {
+    "revision": "7c66218a26df2ce6b95e",
+    "url": "/static/js/2.037f8c2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.037f8c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd756423a02dff3a3ec6",
+    "url": "/static/js/main.17fc4665.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
